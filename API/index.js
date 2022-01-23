0f4f11139faa5f122b78aa6067acc836c2abda0d
NicolasPelletierMulsant <81159047+NicolasPelletierMulsant@users.noreply.github.com>
2022-01-23 22:58:17 +0100

    Initial commit

---
+++ API/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "IoT"
+});
+
+con.connect(err => {
+    if (err) throw err;
+    console.log("Connected!");
+})
+
+app.use(express.json());
+
+app.listen(8080, () => {
+    console.log("Hello");
+})
+
+// User
+
+app.post('/user', (req, res) => {
+    console.log(req.body.username);
+    con.query(`INSERT INTO user (username, password) VALUES ('${req.body.username}', '${req.body.password}')`, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("User " + req.body.username + " added");
+        }
+    });
+    res.send(req.body);
+})
+
+app.post('/user/login', (req, res) => {
+    console.log(req.body);
+    con.query(`SELECT * FROM user WHERE username = '${req.body.username}' AND password = '${req.body.password}'`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(404).send("Error!");
+        } else if (result.length !== 0) {
+            res.status(200).send("User logged in!");
+        } else {
+            res.status(400).send("Wrong username or password!");
+        }
+    });
+})
+
+app.post('/user/logout', (req, res) => {
+    res.send("Not implemented yet.");
+})
+
+app.get('/user/:name', (req, res) => {
+    con.query(`SELECT id, username FROM user WHERE username = '${req.params.name}'`, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else if (result.length === 0) {
+            res.status(404);
+            res.send(`User ${req.params.name} not found!`);
+        } else {
+            console.log(`Fetched user ${result[0].username} (id: ${result[0].id})`);
+            res.send(JSON.stringify( { "id": result[0].id, "username": result[0].username }));
+        }
+    });
+})
+
+app.delete('/user/:name', (req, res) => {
+    con.query(`DELETE FROM user WHERE username = '${req.params.name}'`, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(404).send("Error!");
+        } else if (result.affectedRows === 0) {
+            res.status(400).send("No user found!");
+        } else {
+            console.log(`Deleted user ${req.params.name}`);
+            res.send("Deleted user " + req.params.name);
+        }
+    });
+})
